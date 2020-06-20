99b66bed049941ac734d84737734b9b01e33f407
Amrit Sreekumar <amrit.sreekumar@gmail.com>
2020-06-20 15:05:56 +0530

    Changed response as per requirement

--- app.py
+++ app.py
@@ -6,20 +6,15 @@ class HelloHandler(RequestHandler):
     def get(self):
         print("Request Received")
         messages = whatsapp_web.scrape(None)
-        Dict = {}
-        Dict2 = {}
         i = 0
         for x in messages:
         	if i%2 == 0:
-        		Dict['Anish'] = x
-        		Dict2[i] = Dict
+        		messages[i] = 'Anish -- ' + x
         	else:
-        		Dict['Amrit'] = x
-        		Dict2[i] = Dict
-        	Dict = {}
+        		messages[i] = 'Amrit -- ' + x
         	i = i+1
-        print(Dict2)
-        self.write({'chatlog' : Dict2})
+        print(messages)
+        self.write({'chatlog' : messages})
 
 def make_app():
     urls = [("/", HelloHandler)]
