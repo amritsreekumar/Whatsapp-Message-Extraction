047ded9cbd2739f5b8e79fa471a2cfaa6871b9de
Amrit Sreekumar <amrit.sreekumar@gmail.com>
2020-06-23 20:07:40 +0530

    Added time and xamp support

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from tornado.web import Application, RequestHandler
 from tornado.ioloop import IOLoop
 import whatsapp_web
 import json
+import time
+from datetime import datetime
+
 class HelloHandler(RequestHandler):
     def get(self):
         print("Request Received")
+        initialtime = datetime.now()
         messages = whatsapp_web.scrape(None)
         i = 0
         for x in messages:
@@ -15,6 +19,9 @@ class HelloHandler(RequestHandler):
         	i = i+1
         #print(messages)
         self.write(json.dumps({'chatlog' : messages}))
+        print("Request Completed")
+        finaltime = datetime.now()
+        print(finaltime-initialtime)
 
 def make_app():
     urls = [("/", HelloHandler)]
